93d7f9c3b0b1ad356e23630e2a497bc6cd7df78b
maxpalyvoda <max.palyvoda@gmail.com>
2015-06-08 15:37:30 +0300

    Update task3.js / added password block

--- js/home/lesson2/task3.js
+++ js/home/lesson2/task3.js
@@ -1,6 +1,15 @@
 var userLogin = prompt('Login', '');
 if (userLogin == 'admin') {
-  prompt('Password');
+  var userPassword = prompt ('Password', '');
+    if (userPassword == 'passw0rd') {
+    alert('Welcome home!');
+    }
+    else if (userPassword == null) {
+    alert('Canceled');
+    }
+    else {
+    alert('Wrong password');
+    } 
 }
 else if (userLogin == null) {
   alert('Canceled');
