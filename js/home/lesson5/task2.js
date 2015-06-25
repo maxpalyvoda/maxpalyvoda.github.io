ce17eeea1d0d5da1fb99205d36143e044be25c4d
maxpalyvoda <max.palyvoda@gmail.com>
2015-06-25 16:53:06 +0300

    Create task2.js

---
+++ js/home/lesson5/task2.js
@@ -0,0 +1,36 @@
+ menu = {
+    width: 100,
+    height: 400,
+    title: 'Cool image'
+  };
+
+function isNumeric(n) {
+  return !isNaN(parseFloat(n)) && isFinite(n)
+}
+
+function multiplyNumeric (obj) {
+    for (key in obj) {
+      if (isNumeric(obj[key])) {
+        obj[key] *= 2;
+    }
+  }
+}
+multiplyNumeric(menu);
+console.log(menu);
+
+// вопрос, решил задачу с помощю гугл, как можно решить без написанной проверки
+
+//comments
+
+/* function isNumeric(n) {
+  return !isNaN(parseFloat(n)) && isFinite(n)
+}
+
+Метод parseFloat преобразует строковый аргумент в число с плавающей точкой. 
+Если во время преобразования он обнаруживает неподходящий символ, то заканчивает процесс и возвращает результат.
+Если аргумент не может быть преобразован в число - возвращает NaN
+
+isFinite(number)
+Возвращает false, если аргумент является NaN, положительной или отрицательной бесконечностью. Иначе возвращает true.
+
+*/
