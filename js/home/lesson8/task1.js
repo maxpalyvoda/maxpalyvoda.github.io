d19157f89edf2b2feab262ce3e99f87555666e89
Max Palyvoda <max.palyvoda@gmail.com>
2015-07-06 23:46:59 +0300

    Create task1.js

---
+++ js/home/lesson8/task1.js
@@ -0,0 +1,15 @@
+var ladder = {
+  step: 0,
+  up: function() { // вверх по лестнице
+    this.step++;
+    return this;
+  },
+  down: function() { // вниз по лестнице
+    this.step--;
+    return this;
+  },
+  showStep: function() { // вывести текущую ступеньку
+    alert( this.step );
+  }
+};
+ladder.up().up().down().up().down().showStep();
