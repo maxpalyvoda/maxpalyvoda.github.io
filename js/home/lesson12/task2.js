fc7427841582407aba169778aca6e414e300b14c
Max Palyvoda <max.palyvoda@gmail.com>
2015-07-16 23:12:11 +0300

    Create task2.js

---
+++ js/home/lesson12/task2.js
@@ -0,0 +1,30 @@
+//Добавьте кофеварке геттер для приватного свойства power, чтобы внешний код мог узнать мощность кофеварки.
+
+function CoffeeMachine(power, capacity) {
+  
+  
+  this.setWaterAmount = function(amount) {
+    if (amount < 0) {
+      throw new Error("Значение должно быть положительным");
+    }
+    if (amount > capacity) {
+      throw new Error("Нельзя залить воды больше, чем " + capacity);
+    }
+
+    waterAmount = amount;
+  };
+
+  this.getWaterAmount = function() {
+    return waterAmount;
+  };
+
+  // my getter to return power
+  
+  this.getPower = function() {
+    return power;
+  }
+  
+}
+
+machine1 = new CoffeeMachine(100, 1000);
+alert(machine1.getPower());
