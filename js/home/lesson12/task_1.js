54c066a365c747bba66d27ed3799cc80f2ac4a7a
Max Palyvoda <max.palyvoda@gmail.com>
2015-07-17 16:53:33 +0300

    Create task_1.js

---
+++ js/home/lesson12/task_1.js
@@ -0,0 +1,50 @@
+function Machine(power) {
+  this._enabled = false;
+ // var self = this;
+
+  // on 
+  this.enable = function() {
+       this._enabled = true;
+       console.log('on')
+  };
+  
+// off
+  this.disable = function() {
+    this._enabled = false;
+    console.log('off')
+  };
+
+}
+
+function CoffeeMachine(power) {
+  //apply machine on/off functions with arguments
+  Machine.apply(this, arguments);
+
+  var waterAmount = 0;
+// setter for water amount
+  this.setWaterAmount = function(amount) {
+    waterAmount = amount;
+  };
+
+  /*var parentEnable = this.enable;
+  this.enable = function() {
+      parentEnable(); // теперь можно вызывать как угодно, this не важен
+      this.run();
+    } */
+
+  function onReady() {
+    console.log( 'coffee is ready' );
+  }
+
+  this.run = function() {
+    if (!this._enabled) {
+    throw new Error("Кофеварка выключена");
+  }
+    setTimeout(onReady, 1000);
+  };
+
+}
+
+var coffeeMachine = new CoffeeMachine(10000);
+coffeeMachine.enable();
+coffeeMachine.run();
