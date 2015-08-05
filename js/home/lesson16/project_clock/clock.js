d72aff921dfc7c0eb03c3d86ac33aaf74eaca2e3
Max Palyvoda <max.palyvoda@gmail.com>
2015-08-05 17:32:54 +0300

    Create clock.js

---
+++ js/home/lesson16/project_clock/clock.js
@@ -0,0 +1,32 @@
+  function displayTime() {
+    
+    var currentTime = new Date(); //ask js to give us a date, time, etc.
+    
+   // define variables for getting hours, min, sec from new Date
+    
+    var hours = currentTime.getHours();
+    var minutes = currentTime.getMinutes();
+    var seconds = currentTime.getSeconds();
+    //var milliseconds = currentTime.getMilliseconds();
+    
+  // add zero in front of numbers < 10
+    minutes = checkTime(minutes);
+    seconds = checkTime(seconds);
+    
+  // function for adding 0 in front  
+    function checkTime(i) {
+      if (i<10) {
+        i = "0" + i
+      }  
+        return i;
+}
+    
+    // to appear/connect in/to html
+var clockDiv = document.getElementById('clock').innerHTML = hours + ':' + minutes + ':' + seconds;
+  
+}
+
+
+displayTime(); // call a function
+setInterval(displayTime, 100); // make clock alive
+  
